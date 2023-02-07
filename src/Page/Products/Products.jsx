667b81a9d47b0798fc03f48521e88c68004b5540
amir4976 <amirhosianahmadi4976@gmail.com>
2023-02-07 11:17:00 +0330

    upgrade product table

--- src/Page/Products/Products.jsx
+++ src/Page/Products/Products.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import AddProduct from '../../components/AddProduct/AddProduct'
 import ErrorBox from '../../components/ErrorBox/ErrorBox'
-
+import ProductTable from '../../components/ProductTable/ProductTable'
 
 function Products() {
   return (
     <>
       <AddProduct/>
-      <ErrorBox/>
+      <ErrorBox title={'محصولی'}/>
+      <ProductTable/>
     </>
   )
 }
--- src/components/ProductTable/ProductTable.jsx
+++ src/components/ProductTable/ProductTable.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-import './ProductTable.css'
+import React from "react";
+import "./ProductTable.css";
 function ProductTable() {
   return (
-    <div>ProductTable</div>
-  )
+    <table className="product-table">
+      <tr className="table-product-title-tr">
+        <th>عنوان</th>
+        <th>نام</th>
+        <th>قیمت</th>
+        <th>موجودی</th>
+      </tr>
+
+      <tr className="product-row">
+        <td>
+          <img className="table-item-img" src="./logo192.png" alt="" />
+        </td>
+        <td>lorem</td>
+        <td>1200$</td>
+        <td>150</td>
+        <td className="Table-action-btns-warper">
+          <button className="table-action-btns">جزییات</button>
+          <button className="table-action-btns">حذف</button>
+          <button className="table-action-btns">ویرایش</button>
+        </td>
+      </tr>
+    </table>
+  );
 }
 
-export default ProductTable
+export default ProductTable;
